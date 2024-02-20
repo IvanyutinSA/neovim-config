f8bd0bd3de41a8482dbe4ac6bed5178b1f6b169f
IvanyutinSA <ivanyutin@gmail.com>
2024-02-20 12:09:34 +0600

    added trouble package, changed colorscheme

--- lua/sdf/packer.lua
+++ lua/sdf/packer.lua
@@ -46,4 +46,18 @@ return require('packer').startup(function(use)
 		  require('Comment').setup()
 	  end
   }
+
+  use({
+       "folke/trouble.nvim",
+       config = function()
+           require("trouble").setup {
+               icons = false,
+               -- your configuration comes here
+               -- or leave it empty to use the default settings
+               -- refer to the configuration section below
+           }
+       end
+   })
+
+  use "rebelot/kanagawa.nvim"
 end)
--- lua/sdf/set.lua
+++ lua/sdf/set.lua
@@ -6,10 +6,12 @@ vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 
+vim.opt.smartindent = true
+
+vim.wrap = false
+
 vim.opt.swapfile = false
 vim.opt.backup = false
 
 vim.opt.scrolloff = 8
-
-vim.opt.smartindent = true
 
